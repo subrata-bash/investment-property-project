e3570040c1046ceaee2d71c31ad06a5c57441e9b
Om Sri Guru Joy <github.subrata@gmail.com>
2025-11-10 18:06:46 +0530

    Laravel Multi Auth Breeze Part 2

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,6 +28,11 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
+        $user = $request->user();
+        if ($user->role === 'admin') {
+            return redirect()->route('admin.dashboard');
+        }
+
         return redirect()->intended(route('dashboard', absolute: false));
     }
 
